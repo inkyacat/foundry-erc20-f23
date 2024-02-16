3a0a0811ba0f215a7ebee750f8065c745a0388c6
Kuni <brunt-divides.0f@icloud.com>
2024-02-16 14:16:51 +0100

    Initial commit

---
+++ test/OurTokenTest.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployOurToken} from "../src/OurToken.sol";
+
+contract OurTokenTest is Test {
+    function setUp public {
+
+    }
+}
